a0c8b87902a41cc82d026823dafad8243f38f2af
pranavtharoor <pranav.tharoor@gmail.com>
2018-10-01 23:36:21 +0530

    Fixed config

--- src/core/App/app.spec.js
+++ src/core/App/app.spec.js
@@ -4,7 +4,9 @@ import { shallow } from 'enzyme';
 
 describe('<App />', () => {
   it('renders 1 <App /> component', () => {
-    const component = shallow(<App loggedIn={false} loading={false} />);
+    const component = shallow(
+      <App loggedIn={false} loading={false} level={0} />
+    );
     expect(component).toHaveLength(1);
   });
 });
